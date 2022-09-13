3c7f24198388e016d940a5545a4c027e03b302c9
Olumide Odusote <olumideodusote@gmail.com>
2022-09-13 07:55:16 -0700

    Create a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,16 @@
  *Return: Always 0 (Success)
  */
 
-#define MAX 10
-
 int main(void)
 {
-char name[] = "_putchar";
-for (int count  = 0; count < MAX; count++)
-{
-if (name[count] == '\0')
-{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 putchar('\n');
-break;
-}
-else
-putchar(name[count]);
-}
 return (0);
 }
