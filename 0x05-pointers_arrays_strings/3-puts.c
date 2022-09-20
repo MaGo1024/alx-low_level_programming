a78407ac08bcdb745ab948e3808805ea3136f97c
Olumide Odusote <olumideodusote@gmail.com>
2022-09-20 07:46:49 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_puts - function to print a string
+ *@str: pointer value of character
+ * Return: string.
+ */
+int _puts(char *str)
+{
+return (puts(str));
+}
