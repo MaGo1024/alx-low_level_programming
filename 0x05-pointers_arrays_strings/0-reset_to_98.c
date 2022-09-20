5c9e9dc1ea25f47ec506beecc58712f514cb6673
Olumide Odusote <olumideodusote@gmail.com>
2022-09-20 04:09:06 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - change paramater value to 98 in memory
+ *@n: pointer value
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
