d4d043f52cfcbc8600187e9d6a149f79a972a8df
Olumide Odusote <olumideodusote@gmail.com>
2022-09-20 04:47:39 -0700

    Lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strlen - function to find lenght of paramater value s
+ *@s: pointer value of character
+ * Return: lenght of string.
+ */
+int _strlen(char *s)
+{
+strlen(s);
+}
