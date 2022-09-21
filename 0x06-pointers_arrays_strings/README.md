fd80f283da8c81699229f0fcd28a873ad6496dc1
Olumide Odusote <olumideodusote@gmail.com>
2022-09-21 08:10:18 -0700

    Create a function to concatenate two string

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More on Pointer, String and Array README
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _strcat - concats strings
+ * @dest: destination of string
+ * @src: source of string to concat
+ * Return: returns char value
+ */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
