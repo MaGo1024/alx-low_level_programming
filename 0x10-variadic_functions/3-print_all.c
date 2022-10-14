fba1c0a4823a2ab2c9b459e9d9ef6cb477361a32
Olumide Odusote <olumideodusote@gmail.com>
2022-10-14 07:51:09 -0700

    Create a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,94 +1,51 @@
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_c - prints char
- * @a: list to give
- * Return: always 0
- */
-int print_c(va_list a)
-{
-printf("%c", va_arg(a, int));
-return (0);
-}
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-int print_i(va_list a)
-{
-printf("%d", va_arg(a, int));
-return (0);
-}
-
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-
-int print_f(va_list a)
-{
-printf("%f", va_arg(a, double));
-return (0);
-}
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-int print_s(va_list a)
-{
-char *s;
-s = va_arg(a, char *);
-if (s == NULL)
-{
-printf("(nil)");
-return (0);
-}
-printf("%s", s);
-return (0);
-}
-
-/**
- * print_all - prints all
- * @format: format string that says arg types
+ * print_all - Prints all inputs to the screen
+ * @format: This is a string containing the format
  *
+ * Return: Null Void
  */
 
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *sep = "";
-char *sep2 = ", ";
-va_list anyArgs;
-printer ops[] = {
-{"c", print_c},
-{"i", print_i},
-{"s", print_s},
-{"f", print_f},
-{NULL, NULL}
-};
-va_start(anyArgs, format)
-i = 0;
-while (format != NULL && format[i])
+va_list ap;
+int i = 0, j, c = 0;
+char *str;
+const char fmt[] = "cifs";
+va_start(ap, format);
+while (format && format[i])
 {
 j = 0;
-while (ops[j].f != NULL)
+while (fmt[j])
 {
-if (format[i] == *(ops[j].c))
+if (fmt[j] == format[i] && c)
 {
-printf("%s", sep);
-ops[j].f(anyArgs);
+printf(", ");
+break;
+} j++;
 }
-j++;
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(ap, int)), c = 1;
+break;
+case 'i':
+printf("%i", va_arg(ap, int)), c = 1;
+break;
+case 'f':
+printf("%f", va_arg(ap, double)), c = 1;
+break;
+case 's':
+str = va_arg(ap, char *), c = 1;
+if (!str)
+{
+printf("(nil)");
+break;
 }
-sep = sep2;
-i++;
+printf("%s", str);
+break;
+} i++;
 }
-printf("\n");
-va_end(anyArgs);
+printf("\n"), va_end(ap);
 }
